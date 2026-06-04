52de8061be39cb890a93bc0d1a4a4873d0a19dbb
Daniel Diaz de Leon <daniel.diaz.deleon@outlook.com>
2026-06-03 23:20:08 -0600

    Start working on number spriral

---
+++ Competitive Programming/CSES/Introductory Problems/Number Spiral.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define nl "\n"
+using ll = long long;
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    bool turn = true;
+    
+    int eight_h_multiplier = 0;
+    int starting = 1;
+    while (--t) {
+        int x, y;
+        if (x > y) {
+            int perimeter_x = (x / 2) * 3 + (x / 2  - 1) * 8 + ((x - 1) / 2) * 1;
+        }
+        else {
+            int perimeter_y = ;
+        }
+
+
+        //I get the one on the perimeter
+        //Depending on the level, i go g}form left to right or  up to down
+
+
+
+
+        
+
+        
+
+    }
+    return 0;
+}
